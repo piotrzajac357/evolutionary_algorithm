83c8bccf953a6d4cc91309ade1d4fcdaaadf9478
zajac_piotr <zajac.piotr3579@gmail.com>
2019-12-07 17:26:20 +0100

    .

--- Nowy folder/xd.py
+++ Nowy folder/xd.py
@@ -4,6 +4,7 @@
 from typing import List
 from random import seed, randint, shuffle
 
+
 #   from copy import copy, deepcopy
 
 
@@ -27,28 +28,24 @@ class Garage:
         goal = 0
         cart_number = -1
         last_elem = 0
+        single_cart_time = 0
         for elem in solution:
             if not elem:
+                if single_cart_time > goal:
+                    goal = single_cart_time
                 cart_number += 1
+                single_cart_time = 0
             else:
-                goal += self.coefficients[cart_number] * self.stands[elem - 1][3]
+                single_cart_time += self.coefficients[cart_number] * self.stands[elem - 1][3]
                 if not last_elem:
-                    goal += (self.stands[elem - 1][1] + self.stands[elem - 1][2])
+                    single_cart_time += (self.stands[elem - 1][1] + self.stands[elem - 1][2])
                 else:
-                    goal += (abs(self.stands[elem - 1][1] - self.stands[last_elem - 1][1]) +
-                             abs(self.stands[elem - 1][2] - self.stands[last_elem - 1][2]))
+                    single_cart_time += (abs(self.stands[elem - 1][1] - self.stands[last_elem - 1][1]) +
+                                         abs(self.stands[elem - 1][2] - self.stands[last_elem - 1][2]))
             last_elem = elem
+        if single_cart_time > goal:
+            goal = single_cart_time
         return goal
-
-
-class Solution(Garage):
-
-    def __init__(self, solution: List[int], *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.solution = solution
-
-    def get_solution_as_list(self) -> List[int]:
-        return self.solution
 
 
 def matrix_to_list(matrix: List[List[int]]) -> List[List[int]]:
@@ -64,10 +61,10 @@ def matrix_to_list(matrix: List[List[int]]) -> List[List[int]]:
     return converted
 
 
-def generate_random_input(dim: int = 1, costs_range: int = 1, what_seed: int = 1) -> List[List[int]]:
+def generate_random_input(dim: int = 1, costs_range: int = 1) -> List[List[int]]:
     # funkcja tworzy liste list z losowymi wartosciami naturalnymi od 0 do wartosci rownej drugiemu argumentowi
     matrix = []
-    seed(what_seed)
+    # seed(what_seed)
     for i in range(0, dim):
         matrix.append([])
         for j in range(0, dim):
@@ -82,7 +79,7 @@ def generate_random_solution(number_of_carts: int = 1, problem: List[List[int]] 
     if problem is None:
         problem = [[1]]
     solution = []
-    for i in range(1, len(problem)):
+    for i in range(1, len(problem) + 1):
         solution.append(i)
     shuffle(solution)
     solution.insert(0, 0)
@@ -111,6 +108,7 @@ def generate_random_coefficients_vector(number_of_carts: int = 1, lower_limit: f
     return coefficients_vector
 
 
+seed(1)
 abc = [[0, 0, 2], [3, 4, 0], [6, 0, 8]]
 print("dzialanie matrix_to_list")
 print(matrix_to_list(abc))  # sprawdzenie dzialania matrix_to_list
@@ -118,11 +116,11 @@ print("przykladowa macierz")
 print("[0 7 3 0 0 0 0] \n [0 0 0 0 0 0 0] \n [2 0 0 0 0 0 0] \n [0 0 0 4 0 0 0] \n [0 0 0 0 5 0 0] \n [0 0 0 0 0 0 20]")
 garage = Garage(3, [1, 2, 3], [[1, 0, 2, 2], [2, 2, 0, 3], [3, 3, 3, 4], [4, 4, 4, 5], [5, 5, 6, 20], [6, 0, 1, 7]])
 print("policzona funkcja celu dla 3 wozkow i przypadkowego rozwiazania i wektora wspolczynnikow dla powyzszej macierzy")
-print(garage.goal_function([0, 1, 2, 0, 3, 4, 0, 5, 6]))    # sprawdzenie dzialania goal_function (dziala)
+print(garage.goal_function([0, 1, 2, 0, 3, 4, 0, 5, 6]))  # sprawdzenie dzialania goal_function (dziala)
 
 # macierz losowych naturalnych wartosci 3x3
 print("wygenerowana losowo macierz 5x5 z wartosciami max 10")
-efg = generate_random_input(10, 100)
+efg = generate_random_input(4, 20)
 for p in efg:
     print(p)
 print("wygenerowane losowo 5 rozwiazan do powyzszej macierzy i 4 wozkow")
@@ -130,8 +128,8 @@ abc = generate_random_solutions(4, matrix_to_list(efg), 5)
 for xd in abc:
     print(xd)
 
-print("losowo wygenerowany wektor 10 wspolczynnikow z zakresu 0.5 - 1.5 z krokiem 0.01")
-wsp = generate_random_coefficients_vector(4, 0.5, 1.5, 0.01)
+print("losowo wygenerowany wektor 10 w  spolczynnikow z zakresu 0.5 - 1.5 z krokiem 0.01")
+wsp = generate_random_coefficients_vector(4, 1, 3, 1)
 print(wsp)
 
 print("wyliczenie funkcji celu dla wygenerowanych wczesniej danych")
